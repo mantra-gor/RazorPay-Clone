aac4bea56caacf0c519e2e1489958f9052c723ae
Mantra Gor <mantragor@Mantras-MacBook-Air.local>
2023-03-29 19:34:09 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+      },
+      dropShadow:{
+        '3xl':'0px 0px 25px #00000080'
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+        cyanMG:"#4dd0e1f0",
+        purpleMG: "4D29D1",
+      },},
+  },
+  plugins: [],
+}
